b52006de0280cb0d39f658206407b9732a59a8c5
alien190 <alien1900@bk.ru>
2018-08-08 15:30:10 +0700

    added interface Repository

---
+++ app/src/main/java/com/example/alien/course04task02/data/IRepository.java
@@ -0,0 +1,28 @@
+package com.example.alien.course04task02.data;
+
+import com.example.alien.course04task02.data.model.Film;
+
+import java.util.List;
+
+public interface IRepository {
+
+    long insertItem(Film film);
+
+    Film getItem(long id);
+
+    boolean deleteItem(long id);
+
+    List<Film> getAll();
+
+    void updateItem(Film film);
+
+    List<Film> search(String query);
+
+    List<Film> searchInBounds(int startYear, int endYear);
+
+    List<Film> searchByDirector(String name);
+
+    List<Film> getTopFilms(int count);
+
+
+}
