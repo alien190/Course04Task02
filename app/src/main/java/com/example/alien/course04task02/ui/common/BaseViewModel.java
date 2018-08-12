23646ee4a8c98080fcf4c132f05e48cbb34bdcbc
alien190 <alien1900@bk.ru>
2018-08-12 16:07:13 +0700

    added BaseViewModel

---
+++ app/src/main/java/com/example/alien/course04task02/ui/common/BaseViewModel.java
@@ -0,0 +1,28 @@
+package com.example.alien.course04task02.ui.common;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.alien.course04task02.data.IRepository;
+import com.example.alien.course04task02.data.model.Film;
+
+import java.util.List;
+
+public abstract class BaseViewModel extends ViewModel {
+    protected MutableLiveData<List<Film>> mFilmList = new MutableLiveData<>();
+    private MutableLiveData<Boolean> mIsEmpty = new MutableLiveData<>();
+
+    protected IRepository mRepository;
+
+    public BaseViewModel(IRepository repository) {
+        this.mRepository = repository;
+        mFilmList.observeForever(list -> mIsEmpty.setValue(!(list != null && !list.isEmpty())));
+        mFilmList.setValue(repository.getAll());
+    }
+    public MutableLiveData<List<Film>> getFilmList() {
+        return mFilmList;
+    }
+    public MutableLiveData<Boolean> getIsEmpty() {
+        return mIsEmpty;
+    }
+}
--- app/src/main/java/com/example/alien/course04task02/ui/filmList/FilmListViewModel.java
+++ app/src/main/java/com/example/alien/course04task02/ui/filmList/FilmListViewModel.java
@@ -7,16 +7,16 @@ import android.text.TextWatcher;
 
 import com.example.alien.course04task02.data.IRepository;
 import com.example.alien.course04task02.data.model.Film;
+import com.example.alien.course04task02.ui.common.BaseViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class FilmListViewModel extends ViewModel {
-    private MutableLiveData<List<Film>> mFilmList = new MutableLiveData<>();
-    private MutableLiveData<Boolean> mIsEmpty = new MutableLiveData<>();
+public class FilmListViewModel extends BaseViewModel {
+
     private MutableLiveData<String> mSearchByNameQuery = new MutableLiveData<>();
-    private IRepository mRepository;
+
 
     public TextWatcher watcher = new TextWatcher() {
         @Override
@@ -36,17 +36,12 @@ public class FilmListViewModel extends ViewModel {
     };
 
     public FilmListViewModel(IRepository repository) {
-        this.mRepository = repository;
-        mFilmList.observeForever(list -> mIsEmpty.setValue(!(list != null && !list.isEmpty())));
-        mFilmList.setValue(mRepository.getAll());
+        super(repository);
         mSearchByNameQuery.setValue("dsadas");
-
         // mRepository.createFilmAndSave("meg", "Джон Тёртелтауб", 2018, 5.0);
     }
 
-    public MutableLiveData<List<Film>> getFilmList() {
-        return mFilmList;
-    }
+
 
     public void searchByName(String query) {
         mFilmList.setValue(null);
@@ -54,9 +49,6 @@ public class FilmListViewModel extends ViewModel {
         //mIsEmpty.setValue(true);
     }
 
-    public MutableLiveData<Boolean> getIsEmpty() {
-        return mIsEmpty;
-    }
 
     public MutableLiveData<String> getSearchByNameQuery() {
         return mSearchByNameQuery;
